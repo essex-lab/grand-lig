4fa26b92ec24735c838db04e9e9c5766bc50e238
Marley Samways <mls2g13@soton.ac.uk>
2018-10-10 12:07:34 +0100

    minor update to __init__

--- grand/__init__.py
+++ grand/__init__.py
@@ -1,3 +1,13 @@
+"""
+grand
+
+OpenMM-based implementation of grand canonical Monte Carlo (GCMC) moves to sample water positions
+
+Marley L. Samways
+"""
+
+__version__ = "0.1.0"
+
 import sampler
 import utils
 
