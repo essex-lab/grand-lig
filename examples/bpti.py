a9d0f578a8ac9bf5d4304ab009fabb6d0285d299
Marley Samways <mls2g13@soton.ac.uk>
2018-10-10 13:23:27 +0100

    Updated example

--- examples/bpti.py
+++ examples/bpti.py
@@ -7,23 +7,23 @@ from sys import stdout
 import mdtraj
 from parmed.openmm.reporters import RestartReporter
 
-import gcmc
+import grand
 
 pdb = PDBFile('bpti-equil.pdb')
-pdb.topology, pdb.positions, ghosts = gcmc.utils.add_ghosts(pdb.topology, pdb.positions, n=25, pdb='bpti-gcmc.pdb')
+pdb.topology, pdb.positions, ghosts = grand.utils.add_ghosts(pdb.topology, pdb.positions, n=25, pdb='bpti-gcmc.pdb')
 
 ff = ForceField('amber10.xml', 'tip3p.xml')
 system = ff.createSystem(pdb.topology, nonbondedMethod=PME, nonbondedCutoff=10.0*angstroms,
          constraints=HBonds)
 
 ref_atoms = [['CA', 'TYR', '10'], ['C', 'ASN', '43']]
-gcmc_mover = gcmc.sampler.GrandCanonicalMonteCarloSampler(system=system, topology=pdb.topology, temperature=300*kelvin,
+gcmc_mover = grand.sampler.GrandCanonicalMonteCarloSampler(system=system, topology=pdb.topology, temperature=300*kelvin,
                                                           referenceAtoms=ref_atoms, sphereRadius=4*angstroms)
 
 # Langevin integrator
 integrator = LangevinIntegrator(300*kelvin, 1.0/picosecond, 0.002*picoseconds)
 
-platform = Platform.getPlatformByName('CUDA')
+platform = Platform.getPlatformByName('OpenCL')
 simulation = Simulation(pdb.topology, system, integrator, platform)
 simulation.context.setPositions(pdb.positions)
 simulation.context.setVelocitiesToTemperature(300*kelvin)
@@ -34,13 +34,13 @@ dcd = mdtraj.reporters.DCDReporter('bpti-gcmc.dcd', 0)
 rst7 = RestartReporter('bpti-gcmc.rst7', 0)
 
 # Switch off ghost waters and in sphere
-simulation.context = gcmc_mover.prepareGCMCSphere(simulation.context, ghosts)
-simulation.context = gcmc_mover.deleteWatersInGCMCSphere(simulation.context)
+gcmc_mover.prepareGCMCSphere(simulation.context, ghosts)
+gcmc_mover.deleteWatersInGCMCSphere()
 
 # Equilibrate water distribution
 print("GCMC equilibration...")
 for i in range(75):
-    simulation.context = gcmc_mover.move(simulation.context, 200)  # 200 GCMC moves
+    gcmc_mover.move(simulation.context, 200)  # 200 GCMC moves
     simulation.step(50)  # 100 fs propagation between moves
     print("\t{} GCMC moves completed. N = {}".format(gcmc_mover.n_moves, gcmc_mover.N))
 
@@ -54,7 +54,7 @@ print("\n\nGCMC production")
 for i in range(50):
     # Carry out 100 GCMC moves per 1 ps of MD
     simulation.step(500)
-    simulation.context = gcmc_mover.move(simulation.context, 100)
+    gcmc_mover.move(simulation.context, 100)
     # Write data out
     state = simulation.context.getState(getPositions=True, getVelocities=True)
     dcd.report(simulation, state)
@@ -63,5 +63,5 @@ for i in range(50):
     print("\t{} GCMC moves completed. N = {}".format(gcmc_mover.n_moves, gcmc_mover.N))
 print("{}/{} moves accepted".format(gcmc_mover.n_accepted, gcmc_mover.n_moves))
 
-gcmc.utils.remove_trajectory_ghosts('bpti-gcmc.pdb', 'bpti-gcmc.dcd', 'gcmc-ghost-wats.txt')
+grand.utils.remove_trajectory_ghosts('bpti-gcmc.pdb', 'bpti-gcmc.dcd', 'gcmc-ghost-wats.txt')
 
