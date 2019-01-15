ac7970ac3af1dea180bf5807bf5706b463fd7b96
Marley Samways <mls2g13@soton.ac.uk>
2019-01-15 16:20:51 +0000

    Now reads version from grand/__init__.py

--- setup.py
+++ setup.py
@@ -4,11 +4,18 @@ Setup script to facilitate installation of the GCMC OpenMM scripts
 Marley L. Samways
 """
 
-#from distutils.core import setup
+import os
 from setuptools import setup
 
+
+# Read version number from the __init__.py file
+with open(os.path.join('grand', '__init__.py'), 'r') as f:
+    for line in f.readlines():
+        if '__version__' in line:
+            version = line.split()[-1].strip('"')
+
 setup(name="grand",
-      version="0.1.0",
+      version=version,
       description="OpenMM-based implementation of grand canonical Monte Carlo (GCMC)",
       author="Marley L. Samways",
       author_email="mls2g13@soton.ac.uk",
