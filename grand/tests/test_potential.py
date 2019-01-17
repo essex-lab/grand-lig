590406db73411d861fad0aa8088b99824463156d
Marley Samways <mls2g13@soton.ac.uk>
2019-01-17 16:08:24 +0000

    Extended the unit testing...

--- grand/tests/test_potential.py
+++ grand/tests/test_potential.py
@@ -7,6 +7,9 @@ This file contains functions written to test the functions in the grand.potentia
 
 import unittest
 import numpy as np
+from simtk.unit import *
+from simtk.openmm.app import *
+from simtk.openmm import *
 from grand import potential
 
 
@@ -30,3 +33,57 @@ class TestPotential(unittest.TestCase):
         assert all(np.isclose(potential.get_lambda_values(1.50), [1.0, 1.0]))
         assert all(np.isclose(potential.get_lambda_values(-0.50), [0.0, 0.0]))
         assert all(np.isclose(potential.get_lambda_values(-1.00), [0.0, 0.0]))
+
+    def test_calc_mu(self):
+        """
+        Test that the calc_mu function performs sensibly
+
+        Notes
+        -----
+        Need to add more thorough testing here...
+            - Not exactly sure how to do this...
+            - May need to break the calc_mu() function up a bit to facilitate
+              testing...
+        """
+        # Try to calculate dG for three platforms
+        dG_cuda = None
+        dG_opencl = None
+        dG_cpu = None
+        # Try running on CUDA
+        try:
+            dG_cuda = potential.calc_mu(model='tip3p', box_len=25 * angstroms, cutoff=12 * angstroms, switch_dist=10 * angstroms,
+                                        nb_method=PME, temperature=300 * kelvin, equil_time=20 * femtoseconds,
+                                        sample_time=10 * femtoseconds,
+                                        n_lambdas=6, n_samples=5, dt=2 * femtoseconds,
+                                        platform=Platform.getPlatformByName('CUDA'))
+        except:
+            pass
+        # Then try OpenCL...
+        try:
+            dG_opencl = potential.calc_mu(model='tip3p', box_len=25 * angstroms, cutoff=12 * angstroms,
+                                          switch_dist=10 * angstroms,
+                                          nb_method=PME, temperature=300 * kelvin, equil_time=20 * femtoseconds,
+                                          sample_time=10 * femtoseconds,
+                                          n_lambdas=6, n_samples=5, dt=2 * femtoseconds,
+                                          platform=Platform.getPlatformByName('OpenCL'))
+        except:
+            pass
+        # Then try CPU (default)
+        try:
+            dG_cpu = potential.calc_mu(model='tip3p', box_len=25 * angstroms, cutoff=12 * angstroms,
+                                       switch_dist=10 * angstroms,
+                                       nb_method=PME, temperature=300 * kelvin, equil_time=20 * femtoseconds,
+                                       sample_time=10 * femtoseconds,
+                                       n_lambdas=6, n_samples=5, dt=2 * femtoseconds)
+        except:
+            pass
+        # Check that at least the CPU version has worked...
+        assert dG_cpu is not None
+
+        # Check that those which have worked each returned a free energy
+        for dG in [dG_cuda, dG_opencl, dG_cpu]:
+            if dG is not None:
+                # Make sure that the returned value has units
+                assert isinstance(dG, Quantity)
+                # Make sure that the value has units of energy
+                assert dG.unit.is_compatible(kilocalorie_per_mole)
--- grand/tests/test_utils.py
+++ grand/tests/test_utils.py
@@ -7,6 +7,7 @@ This file contains functions written to test the functions in the grand.utils su
 
 import os
 import unittest
+import numpy as np
 from grand import utils
 
 
@@ -22,3 +23,17 @@ class TestUtils(unittest.TestCase):
         assert os.path.isfile(utils.get_file('tip3p.pdb'))
         # Check that a made up file raises an exception
         self.assertRaises(Exception, lambda: utils.get_file('imaginary.file'))
+
+    def test_rotation_matrix(self):
+        """
+        Test that the random_rotation_matrix functions works as expected
+        """
+        R = utils.random_rotation_matrix()
+        # Matrix must be 3x3
+        assert R.shape == (3, 3)
+        # Make sure that det(R) is +/- 1
+        assert np.isclose(np.linalg.det(R), 1.0) or np.isclose(np.linalg.det(R), -1.0)
+        # Check that the inverse is equal to the transpose of R
+        assert np.all(np.isclose(np.linalg.inv(R), R.T))
+        # Make sure that a different matrix is returned each time
+        assert not np.all(np.isclose(R, utils.random_rotation_matrix()))
